447cdadc860208a24ada22339aba303995b8e772
Amr Yousry <amryousry19@gmail.com>
2022-09-03 21:56:33 -0400

    API Exercise Two

---
+++ CurrentWeatherAPI/OpeanWeatherResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace CurrentWeatherAPI
+{
+    public class OpeanWeatherResponse
+    {
+        public Main Main { get; set; }
+    }
+
+    public class WeatherDescription
+    {
+        public string Main { get; set; }
+    }
+
+    public class Main
+    {
+        public string Temp { get; set; }
+    }
+}
---
+++ CurrentWeatherAPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace CurrentWeatherAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=jacksonville&appid=785e70a516418c2508b78cd5ce39e1f2&units=imperial";
+
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(weatherURL).Result;
+
+            var result = JsonConvert.DeserializeObject<OpeanWeatherResponse>(response);
+
+            var temp = result.Main.Temp;
+
+            Console.WriteLine($"The temperature in Jacksonville is: {temp}");
+        }
+    }
+}
